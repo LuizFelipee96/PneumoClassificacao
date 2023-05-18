f3733aa0f0aa0113b5a4ce9e2e6de73f7499d951
Luiz Felipe <52084917+LuizFelipee96@users.noreply.github.com>
2023-05-17 23:05:37 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import streamlit as st
+from keras.models import load_model
+from PIL import Image
+from util import classify, set_background
+
+st.title('Classificação Pneumonia')
+
+
+st.header('Faça o upload da imagem de raio x do torax de seu paciente')
+
+file = st.file_uploader('', type=['jpeg', 'jpg', 'png'])
+
+model = load_model('./model/pneumonia_classifier.h5')
+
+with open('./model/labels.txt', 'r') as f:
+    class_names = [a[:-1].split(' ')[1] for a in f.readlines()]
+    f.close()
+
+if file is not None:
+    image = Image.open(file).convert('RGB')
+    st.image(image, use_column_width=True)
+    
+    class_name, conf_score = classify(image, model, class_names)
+
+    st.write("## {}".format(class_name))
+    st.write("### score: {}".format(conf_score))
